21bc5093a71e80ac9efa3eb4e95ae2a47079bd89
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2019-07-05 00:45:36 +0200

    Add forms

---
+++ src/Form/DiscountEditType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Discount;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DiscountEditType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', null, [
+                'label' => 'Nazwa',
+            ])
+            ->add('visualName', null, [
+                'label' => 'Nazwa w ofercie',
+                'help' => 'Ta nazwa jest widoczna tylko w wygenerowanej ofercie. Jeśli jej brak, używana jest główna nazwa.',
+            ])
+            ->add('type', ChoiceType::class, [
+                'label' => 'Rodzaj rabatu',
+                'choices' => [
+                    'Zmniejszaj o taką kwotę' => Discount::TYPE_STATIC,
+                    'Zmniejszaj o tyle procent' => Discount::TYPE_PERCENT,
+                ],
+                'expanded' => true,
+            ])
+            ->add('value', null, [
+                'label' => 'Wartość',
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Zapisz rabat',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Discount::class,
+        ]);
+    }
+}
---
+++ src/Form/ItemEditType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Item;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ItemEditType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', null, [
+                'label' => 'Nazwa',
+            ])
+            ->add('visualName', null, [
+                'label' => 'Nazwa w ofercie',
+                'help' => 'Ta nazwa jest widoczna tylko w wygenerowanej ofercie. Jeśli jej brak, używana jest główna nazwa.',
+            ])
+            ->add('price', null, [
+                'label' => 'Cena',
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Zapisz pozycję',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Item::class,
+        ]);
+    }
+}

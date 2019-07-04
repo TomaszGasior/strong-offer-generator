bf20a18902c4e9214c92027f644388fd971b515c
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2019-07-04 20:56:17 +0200

    Add offer model

---
+++ src/Model/Offer.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Model;
+
+use App\Entity\Author;
+use App\Entity\Discount;
+use App\Entity\Item;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Offer
+{
+    /**
+     * @Assert\NotBlank
+     * @Assert\Type(Author::class)
+     */
+    private $author;
+
+    /**
+     * @Assert\NotBlank
+     * @Assert\Type("array")
+     * @Assert\All({@Assert\Type(Discount::class)})
+     */
+    private $discounts = [];
+
+    /**
+     * @Assert\NotBlank
+     * @Assert\Type("array")
+     * @Assert\All({@Assert\Type(Item::class)})
+     */
+    private $items = [];
+
+    public function getAuthor(): Author
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(Author $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getDiscounts(): array
+    {
+        return $this->discounts;
+    }
+
+    public function setDiscounts(array $discounts): self
+    {
+        $this->discounts = $discounts;
+
+        return $this;
+    }
+
+    public function getItems(): array
+    {
+        return $this->items;
+    }
+
+    public function setItems(array $items): self
+    {
+        $this->items = $items;
+
+        return $this;
+    }
+}

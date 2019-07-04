9633e8070ee7ce628e2dc46fee45b62320792b4c
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2019-07-04 22:32:47 +0200

    Add data fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,71 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Author;
+use App\Entity\Discount;
+use App\Entity\Item;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Provider\Base;
 
 class AppFixtures extends Fixture
 {
+    private $faker;
+
+    public function __construct($value='')
+    {
+        $this->faker = Factory::create('pl_PL');
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $f = $this->faker;
+
+        for ($i=0; $i < 3; $i++) {
+            $author = new Author;
+
+            $author->setName($f->name);
+            $author->setEmail($f->safeEmail);
+            $author->setPhone($f->phoneNumber);
+
+            $manager->persist($author);
+        }
+
+        $discount = new Discount;
+        $discount->setName('Rabat za odnośnik w stopce');
+        $discount->setValue(100);
+        $discount->setType(Discount::TYPE_STATIC);
+        $manager->persist($discount);
+
+        $discount = new Discount;
+        $discount->setName('Rabat za zapłatę z góry');
+        $discount->setValue(10);
+        $discount->setType(Discount::TYPE_PERCENT);
+        $manager->persist($discount);
+
+        $itemsNames = [
+            'Gotowy motyw graficzny',
+            'Dedykowany projekt graficzny',
+            'Galeria zdjęć',
+            'Galeria zaawansowana — zdjęcia i wideo',
+            'Blog lub aktualności',
+            'Formularz kontaktowy',
+            'Mapa Google',
+            'Widżet Facebooka',
+            'Integracja z Google Analytics',
+            'Hosting na 12 miesięcy',
+            'Certyfikat zabezpieczeń SSL',
+        ];
+
+        foreach ($itemsNames as $itemName) {
+            $item = new Item;
+
+            $item->setName($itemName);
+            $item->setPrice($f->randomNumber(2));
+
+            $manager->persist($item);
+        }
 
         $manager->flush();
     }

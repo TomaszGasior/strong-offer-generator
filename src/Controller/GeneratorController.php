5004d2e60d1fa83468d22305db8f654ba4836ef4
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2019-07-05 14:00:45 +0200

    GeneratorController: generate PDF file

--- src/Controller/GeneratorController.php
+++ src/Controller/GeneratorController.php
@@ -3,10 +3,13 @@
 namespace App\Controller;
 
 use App\Form\GeneratorJobType;
+use App\Offer\Calculation;
 use App\Offer\Offer;
+use App\Renderer\PdfOfferRenderer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GeneratorController extends AbstractController
@@ -22,15 +25,26 @@ class GeneratorController extends AbstractController
     /**
      * @Route("/generator", name="generator.form")
      */
-    public function form(Request $request): Response
+    public function form(Request $request, PdfOfferRenderer $renderer): Response
     {
         $offer = new Offer;
+        $calculation = new Calculation($offer);
 
-        $form = $this->createForm(GeneratorJobType::class, $offer);
+        $renderer->setOfferData($offer, $calculation);
+
+        $form = $this->createForm(GeneratorJobType::class, $offer, ['method' => 'GET']);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            dd($offer);
+            $response = new Response($renderer->generate());
+
+            $response->headers->set('Content-Disposition', $response->headers->makeDisposition(
+                ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+                'offer_' . date('Y-m-d') . '.pdf',
+            ));
+            $response->headers->set('Content-Type', 'application/pdf');
+
+            return $response;
         }
 
         return $this->render('app/generator-form.html.twig', [

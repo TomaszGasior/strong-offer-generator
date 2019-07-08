8ba2e06a9cf08ab37e74030bcc52e8d8cbff3b26
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2019-07-08 22:48:48 +0200

    AppExtension: make format_price filter

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -9,10 +9,16 @@ use Twig\TwigFunction;
 
 class AppExtension extends AbstractExtension
 {
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('format_price', [Formatter::class, 'formatPrice']),
+        ];
+    }
+
     public function getFunctions(): array
     {
         return [
-            new TwigFunction('format_price', [Formatter::class, 'formatPrice']),
             new TwigFunction('format_discount_value', [Formatter::class, 'formatDiscountValue']),
         ];
     }

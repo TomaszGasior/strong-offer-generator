f01ca5274b19f06fabcbf3193602d75e34585363
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2019-07-08 21:28:05 +0200

    Use OfferFactory in PreviewController

--- src/Controller/PreviewController.php
+++ src/Controller/PreviewController.php
@@ -5,65 +5,28 @@ namespace App\Controller;
 use App\Form\GeneratorJobType;
 use App\Offer\Calculation;
 use App\Offer\Offer;
-use App\Offer\Recipient;
+use App\Offer\OfferFactory;
 use App\Renderer\PdfOfferRenderer;
-use App\Repository\AuthorRepository;
-use App\Repository\DiscountRepository;
-use App\Repository\ItemRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PreviewController extends AbstractController
 {
-    private $authorRespository;
-    private $discountRespository;
-    private $itemRespository;
-
-    public function __construct(AuthorRepository $authorRespository,
-                                DiscountRepository $discountRespository,
-                                ItemRepository $itemRespository)
-    {
-        $this->authorRespository = $authorRespository;
-        $this->discountRespository = $discountRespository;
-        $this->itemRespository = $itemRespository;
-    }
-
     /**
      * @Route("/podglad")
+     * @Route("/generator/podglad")
      */
-    public function preview(Request $request, PdfOfferRenderer $renderer): Response
+    public function preview(PdfOfferRenderer $renderer, OfferFactory $factory): Response
     {
-        $offer = new Offer;
+        $offer = $factory->createPreviewOffer();
         $calculation = new Calculation($offer);
 
         $renderer->setOfferData($offer, $calculation);
-
-        $this->setUpFakeData($offer);
 
         $response = new Response($renderer->generate());
         $response->headers->set('Content-Type', 'application/pdf');
 
         return $response;
-    }
-
-    private function setUpFakeData(Offer $offer): void
-    {
-        $author = $this->authorRespository->findAll()[0];
-        $discounts = $this->discountRespository->findAll();
-        $items = $this->itemRespository->findAll();
-
-        $recipient = new Recipient;
-        $recipient->setCompany('Company Corp.');
-        $recipient->setName('John Smith');
-
-        $expirationDate = new \DateTime('+14 days');
-
-        $offer->setAuthor($author);
-        $offer->setDiscounts($discounts);
-        $offer->setItems($items);
-        $offer->setRecipient($recipient);
-        $offer->setExpirationDate($expirationDate);
     }
 }
--- src/Offer/OfferFactory.php
+++ src/Offer/OfferFactory.php
@@ -35,4 +35,22 @@ class OfferFactory
 
         return $offer;
     }
+
+    public function createPreviewOffer(): Offer
+    {
+        $offer = $this->createBlankOffer();
+
+        $author = $this->authorRespository->findOneBy([]);
+        $discounts = $this->discountRespository->findAll();
+        $items = $this->itemRespository->findAll();
+
+        $offer->setAuthor($author);
+        $offer->setDiscounts($discounts);
+        $offer->setItems($items);
+
+        $offer->getRecipient()->setCompany('Company Corp.');
+        $offer->getRecipient()->setName('John Smith');
+
+        return $offer;
+    }
 }

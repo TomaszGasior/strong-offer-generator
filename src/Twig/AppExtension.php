42adc40e0f0377d40a0229f6a5610961e1f7f98f
Tomasz Gąsior <kontakt@tomaszgasior.pl>
2019-07-08 22:56:24 +0200

    AppExtension: add localized_date filter

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -3,6 +3,7 @@
 namespace App\Twig;
 
 use App\Util\Formatter;
+use Twig\Environment;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 use Twig\TwigFunction;
@@ -13,6 +14,7 @@ class AppExtension extends AbstractExtension
     {
         return [
             new TwigFilter('format_price', [Formatter::class, 'formatPrice']),
+            new TwigFilter('localized_date', [$this, 'localizedDate'], ['needs_environment' => true]),
         ];
     }
 
@@ -21,5 +23,15 @@ class AppExtension extends AbstractExtension
         return [
             new TwigFunction('format_discount_value', [Formatter::class, 'formatDiscountValue']),
         ];
+    }
+
+    public function localizedDate(Environment $twig, $date): string
+    {
+        $date = twig_date_converter($twig, $date);
+
+        $formatter = new \IntlDateFormatter(null, null, null);
+        $formatter->setPattern('d MMMM Y');
+
+        return $formatter->format($date->getTimestamp());
     }
 }
